c162d1f87b9a3e287164603d6a04b7b1b1b0b664
tlagornjs <tlagornjs@gmail.com>
2019-05-10 10:20:21 +0900

    Updates

---
+++ precache-manifest.820dc28ae2c4798eeef731e5e595fb25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd74171fcffd5485b518",
+    "url": "/reactmovie/static/js/runtime~main.fd74171f.js"
+  },
+  {
+    "revision": "5cf09ff20619dee18474",
+    "url": "/reactmovie/static/js/main.5cf09ff2.chunk.js"
+  },
+  {
+    "revision": "999a22de9f8e2866e588",
+    "url": "/reactmovie/static/js/1.999a22de.chunk.js"
+  },
+  {
+    "revision": "5cf09ff20619dee18474",
+    "url": "/reactmovie/static/css/main.d78e8c42.chunk.css"
+  },
+  {
+    "revision": "4ce6073d7a7008656876fdb3d0ebb132",
+    "url": "/reactmovie/index.html"
+  }
+];
